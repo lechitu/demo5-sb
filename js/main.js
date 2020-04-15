1cd462c01f73a04e7ffc9ed7d95604ed532aa156
tu <lechitu1103@gmail.com>
2020-04-15 17:58:01 +0700

    2

--- js/main.js
+++ js/main.js
@@ -1,34 +1,36 @@
 //menu
-(function($){
-	$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-	  if (!$(this).next().hasClass('show')) {
-		$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-	  }
-	  var $subMenu = $(this).next(".dropdown-menu");
-	  $subMenu.toggleClass('show');
+(function ($) {
+    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
 
-	  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-		$('.dropdown-submenu .show').removeClass("show");
-	  });
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+            $('.dropdown-submenu .show').removeClass("show");
+        });
 
-	  return false;
-	});
+        return false;
+    });
 })(jQuery)
 
 //slide main
 $('.owl1').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 })
@@ -43,4 +45,135 @@ $(document).ready(function () {
         preloader: false,
         fixedContentPos: false
     });
+});
+
+//form option
+function form_option1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+function form_option2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+function form_option3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+//slide sign up
+$('.owl2').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    dots: false,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+//slide story
+$('.owl3').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+//slide achievement
+$('.owl4').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+//slide partner
+$('.owl5').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+// fix menu top 
+window.onscroll = function() {
+    console.log(window.pageYOffset);
+    var nav = document.getElementById('menu-header');
+    if (window.pageYOffset > 100) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+}
+
+//return to top
+new WOW().init();
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) { // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(1000); // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(500); // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() { // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
 });
